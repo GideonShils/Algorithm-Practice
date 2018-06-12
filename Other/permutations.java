a024d36e24c58970cdcb6c17a446819042000803
Gideon Shils <gideon.shils@gmail.com>
2018-06-12 02:02:10 -0400

    permutations

---
+++ Other/permutations.java
@@ -0,0 +1,34 @@
+// Get all permutations of a string
+
+import java.util.*;
+
+public class permutations {
+    // Top down approach. Break off last char, recurse on remainder,
+    // insert in all locs, merge 
+	static HashSet<String> permute (String str) {
+        HashSet<String> set = new HashSet<String>();
+        if (str.length() == 0) {
+            set.add("");
+            return set;
+        }
+
+        char letter = str.charAt(str.length() - 1);
+        HashSet<String> strs = permute(str.substring(0, str.length() - 1));
+        for (String s : strs) {
+            for (int i = 0; i <= s.length(); i++) {
+                set.add(s.substring(0,i) + letter + s.substring(i));
+            }
+        }
+        return set;
+	}
+
+	public static void main(String args[]) {
+		String str = "abc";
+
+        HashSet<String> perms = permute(str);
+        
+        for (String s : perms) {
+            System.out.println(s);
+        }
+	}
+}
