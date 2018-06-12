32ec99798e13ee2ef61d1a641666b9a518f1f396
Gideon Shils <gideon.shils@gmail.com>
2018-06-12 14:05:10 -0400

    Update structure

---
+++ CTCI/01-arrays-and-strings/p7.java
@@ -0,0 +1,19 @@
+// Rotate NxN image matrix by 90 degrees where each pixel is 4 bytes.
+import java.util.*;
+
+public class p7 {
+	static int[][] rotate(int[][] img) {
+		
+
+		return img;
+	}
+
+	public static void main(String args[]) {
+		int[][] img = {{ 1,  2,  3,  4},
+					   { 5,  6,  7,  8},
+					   { 9, 10, 11, 12},
+					   {13, 14, 15, 16}};
+
+		System.out.println(rotate(img));
+	}
+}
