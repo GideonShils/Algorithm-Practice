23de26ef50f135234516c43877db518ab4b0b374
Gideon Shils <gideon.shils@gmail.com>
2018-10-26 21:53:43 -0400

    add bottom up DP

--- CTCI/recursion_and_dynamic_programming/TripleStep.java
+++ CTCI/recursion_and_dynamic_programming/TripleStep.java
@@ -18,6 +18,21 @@ public class TripleStep {
         }
     }
 
+    // Bottom up
+    public static int tripleStep(int n) {
+        int[] memo = new int[n + 1];
+
+        memo[0] = 1;
+        memo[1] = 1;
+        memo[2] = 2;
+        
+        for (int i = 3; i < memo.length; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+        }
+
+        return memo[n];
+    }
+
     // Driver
     public static void main(String[] args) {
         int n = 10;
