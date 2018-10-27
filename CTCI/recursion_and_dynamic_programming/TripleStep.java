c7a7bf21d31c73d4d5d0477f2138955e47fdf2bc
Gideon Shils <gideon.shils@gmail.com>
2018-10-26 21:37:11 -0400

    Add recursive

---
+++ CTCI/recursion_and_dynamic_programming/TripleStep.java
@@ -0,0 +1,25 @@
+// A child can run up n stairs 1, 2, or 3 at a time
+// Return the count of all possible ways the child can run up the stairs
+import java.util.*;
+
+public class TripleStep {
+
+    public static int tripleStep(int n, int[] memo) {
+        if (n <= 0) {
+            return 0;
+        } else if (memo[n] > -1) {
+            return memo[n];
+        } else {
+            memo[n] = tripleStep(n - 1, memo) + tripleStep(n - 2, memo) + tripleStep(n-3, memo) + 1;
+            return memo[n];
+        }
+    }
+
+    // Driver
+    public static void main(String[] args) {
+        int n = 10;
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        System.out.println("Ways to run up " + n + " steps: " + tripleStep(n, memo));
+    }
+}
--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # Solutions to Algorithm Practice Problems
 
-Problems from:
-
-1. [Cracking the Coding Interview](https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/0984782850/ref=sr_1_2?ie=UTF8&qid=1528775471&sr=8-2&keywords=cracking+the+coding+interview)
-
-2. [Leetcode](www.leetcode.com)
-
-3. [Daily Coding Problems](https://www.dailycodingproblem.com)
-
-4. [Geeks for Geeks](https://www.geeksforgeeks.org)
+Most problems from [Cracking the Coding Interview](https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/0984782850/ref=sr_1_2?ie=UTF8&qid=1528775471&sr=8-2&keywords=cracking+the+coding+interview)
