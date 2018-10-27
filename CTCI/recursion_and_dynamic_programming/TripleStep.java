5af9d668a669db34570309bfbb2ea4594a6de6fa
Gideon Shils <gideon.shils@gmail.com>
2018-10-26 21:49:44 -0400

    Fix recursion

--- CTCI/recursion_and_dynamic_programming/TripleStep.java
+++ CTCI/recursion_and_dynamic_programming/TripleStep.java
@@ -4,13 +4,16 @@ import java.util.*;
 
 public class TripleStep {
 
+    // Top down
     public static int tripleStep(int n, int[] memo) {
-        if (n <= 0) {
+        if (n < 0) {
             return 0;
+        } else if (n == 0) {
+            return 1;
         } else if (memo[n] > -1) {
             return memo[n];
         } else {
-            memo[n] = tripleStep(n - 1, memo) + tripleStep(n - 2, memo) + tripleStep(n-3, memo) + 1;
+            memo[n] = tripleStep(n - 1, memo) + tripleStep(n - 2, memo) + tripleStep(n-3, memo);
             return memo[n];
         }
     }
