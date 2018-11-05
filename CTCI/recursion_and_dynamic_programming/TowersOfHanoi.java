af7bc0f3e6e750aab3f045572f365329b148bc25
Gideon Shils <gideon.shils@gmail.com>
2018-11-05 16:22:23 -0500

    Towers of Hanoi

--- CTCI/recursion_and_dynamic_programming/TowersOfHanoi.java
+++ CTCI/recursion_and_dynamic_programming/TowersOfHanoi.java
@@ -9,14 +9,73 @@ import java.util.*;
 
 public class TowersOfHanoi {
 
-    public static int towersOfHanoi(int  n) {
+    static class Tower {
+        Stack<Integer> stack;
 
+        public Tower() {
+            stack = new Stack<Integer>();
+        }
+
+        public void addDisk(int i) {
+            stack.push(i);
+        }
+
+        public void moveTop(Tower t) {
+            t.addDisk(stack.pop());
+        }
+
+        public void move(int numToMove, Tower buffer, Tower destination) {
+            if (numToMove <= 0) {
+                return;
+            }
+
+            // Move n - 1 to buffer
+            move(numToMove - 1, destination, buffer);
+            // Move the top to destination
+            moveTop(destination);
+            // Move n - 1 from buffer to destination, using current as buffer
+            buffer.move(numToMove - 1, this, destination);
+        }
+
+        public void printTower() {
+            for (Integer disk : this.stack) {
+                System.out.println(disk);
+            }
+            System.out.println();
+        }
     }
 
     // Driver
     public static void main(String[] args) {
-        int a = 6;
-        int b = 3;
-        System.out.println(multiply(a, b));
+        int numDisks = 6;
+
+        Tower one = new Tower();
+        Tower two = new Tower();
+        Tower three = new Tower();
+
+        for (int i = numDisks - 1; i >= 0; i--) {
+            one.addDisk(i);
+        }
+
+        // Print original towers
+        System.out.println("Original:");
+        System.out.println("Tower 1: ");
+        one.printTower();
+        System.out.println("Tower 2: ");
+        two.printTower();
+        System.out.println("Tower 3: ");
+        three.printTower();
+
+        // Move disks
+        one.move(numDisks, two, three);
+
+        // Print towers after move
+        System.out.println("After move:");
+        System.out.println("Tower 1: ");
+        one.printTower();
+        System.out.println("Tower 2: ");
+        two.printTower();
+        System.out.println("Tower 3: ");
+        three.printTower();
     }
 }
